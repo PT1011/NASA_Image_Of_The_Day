b62f9f17de43a0c999ded82d937ff515ec0ec4be
PT1011 <parthpthite@gmail.com>
2026-06-06 20:24:23 -0400

    Added react structure to code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-const API_KEY = process.env.REACT_APP_NASA_KEY
+import logo from './logo.svg';
+import './App.css';
 
-fetch("https://api.nasa.gov/planetary/apod")
-    .then(response => {
+function App() {
+    const API_KEY = process.env.REACT_APP_NASA_KEY
+
+    fetch("https://api.nasa.gov/planetary/apod?api_key=" + API_KEY)
+        .then(response => {
         if (!response.ok){
             throw new Error("Could not fetch resource")
         }
         return response.json();
-    })
-    .then((data) => console.log(data))
-    .catch(error => console.error(error));
+        }
+    )
+        .then((data) => console.log(data))
+        .catch(error => console.error(error));
+    return (
+        <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+            </a>
+        </header>
+        </div>
+    );
+}
+
+export default App;
