e24486d31e2927dcfa614cb92ddb728737a1322a
PT1011 <parthpthite@gmail.com>
2026-06-06 20:11:39 -0400

    Started working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+const API_KEY = process.env.REACT_APP_NASA_KEY
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+fetch("https://api.nasa.gov/planetary/apod")
+    .then(response => {
+        if (!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+        return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch(error => console.error(error));
