9ab9b53c7cafccef581a5978ca1ca3e040b6c3e7
PT1011 <parthpthite@gmail.com>
2026-06-06 20:43:57 -0400

    Added useEffect to function, prepairing to add useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
     const API_KEY = process.env.REACT_APP_NASA_KEY
 
-    fetch("https://api.nasa.gov/planetary/apod?api_key=" + API_KEY)
-        .then(response => {
-        if (!response.ok){
-            throw new Error("Could not fetch resource")
-        }
-        return response.json();
-        }
-    )
+    useEffect(() => {
+            fetch("https://api.nasa.gov/planetary/apod?api_key=" + API_KEY)
+            .then(response => {
+            if (!response.ok){
+                throw new Error("Could not fetch resource")
+            }
+            return response.json();
+            }
+        )
         .then((data) => console.log(data))
         .catch(error => console.error(error));
+    }, [])
     return (
         <div className="App">
         <header className="App-header">
